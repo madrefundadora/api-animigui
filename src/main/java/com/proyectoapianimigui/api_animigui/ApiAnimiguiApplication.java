03f132ca0757ee3401ed468e0d3cff063a641950
madrefundadora <martinmendezpaula@gmail.com>
2025-12-10 09:52:56 +0000

    subir el repositorio

---
+++ src/main/java/com/proyectoapianimigui/api_animigui/ApiAnimiguiApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoapianimigui.api_animigui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAnimiguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAnimiguiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoapianimigui/api_animigui/ApiAnimiguiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoapianimigui.api_animigui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAnimiguiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
